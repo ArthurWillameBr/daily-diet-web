3699a26984fa7f8066c222950a0dfa7866074f6c
ArthurWillameBr <arthurwillame2005@gmail.com>
2024-11-20 19:16:57 -0300

    feat: add home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,78 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { ArrowUpRight, Plus, Settings, Utensils } from "lucide-react";
+
 export function Home() {
-  return <div className="bg-secondary h-screens">home</div>;
+  return (
+    <main className="flex flex-col h-screen max-w-6xl mx-auto px-5">
+      <div className="flex items-center justify-between p-5 md:p-8">
+        <div className="flex items-center gap-2">
+          <Utensils className="w-6 h-6 md:w-8 md:h-8" />
+          <div className="flex flex-col leading-3">
+            <p className="text-base md:text-lg">Daily</p>
+            <p className="text-base md:text-lg font-semibold">Diet</p>
+          </div>
+        </div>
+        <div>
+          <Settings className="w-6 h-6 md:w-8 md:h-8" />
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center p-4 md:p-8">
+        <Card className="w-full max-w-[450px] md:max-w-[600px] relative rounded-lg">
+          <CardContent className="bg-[#E5F0DB] text-center">
+            <div className="relative">
+              <ArrowUpRight className="absolute top-2 right-2 text-lime-500 w-6 h-6 md:w-8 md:h-8" />
+            </div>
+            <div className="p-5 md:p-8">
+              <h2 className="text-2xl md:text-4xl font-semibold">98.3%</h2>
+              <p className="md:text-lg">das refeições dentro da dieta</p>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+
+      <div className="px-4 md:px-8 py-2 md:py-4 space-y-3">
+        <h2 className="font-semibold text-lg md:text-xl">Refeições</h2>
+        <Button className="w-full h-12 md:h-14 text-base md:text-lg">
+          <Plus className="mr-2 w-5 h-5 md:w-6 md:h-6" /> Nova refeição
+        </Button>
+      </div>
+
+      <ScrollArea className="flex-1 px-4 md:px-8">
+        <div className="space-y-6 pb-6 md:pb-8">
+          <div className="space-y-4">
+            <h2 className="font-medium pt-4 text-base md:text-lg">12.02.24</h2>
+            {[1, 2, 3].map((_, index) => (
+              <Card key={index} className="shadow-md">
+                <CardContent className="flex items-center p-4 md:p-5">
+                  <span className="text-sm md:text-base">20:00</span>
+                  <div className="w-[1px] h-4 mx-4 bg-slate-500" />
+                  <span className="flex-1 text-sm md:text-base">
+                    Frango grelhado
+                  </span>
+                  <div className="bg-[#E5F0DB] rounded-full size-4 md:size-5" />
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+          <div className="space-y-4">
+            <h2 className="font-medium text-base md:text-lg">11.02.24</h2>
+            {[1, 2, 3].map((_, index) => (
+              <Card key={index} className="shadow-md">
+                <CardContent className="flex items-center p-4 md:p-5">
+                  <span className="text-sm md:text-base">20:00</span>
+                  <div className="w-[1px] h-4 mx-4 bg-slate-500" />
+                  <span className="flex-1 text-sm md:text-base">
+                    Frango grelhado
+                  </span>
+                  <div className="bg-[#E5F0DB] rounded-full size-4 md:size-5" />
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </ScrollArea>
+    </main>
+  );
 }
