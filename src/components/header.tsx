c4c2eeeaf13b3317e4455149903ebf12736c556f
ArthurWillameBr <arthurwillame2005@gmail.com>
2024-11-20 12:00:12 -0300

    feat: add header component

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+import { Utensils } from "lucide-react";
+
+export function Header() {
+  return (
+    <div className="w-full flex gap-2 pl-4 pt-4">
+      <Utensils /> Daily Diet
+    </div>
+  );
+}
