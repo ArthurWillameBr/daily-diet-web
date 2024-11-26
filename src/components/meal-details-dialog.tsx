48b522b5d74500ff3135aa07858261efd61734b4
ArthurWillameBr <arthurwillame2005@gmail.com>
2024-11-26 09:49:47 -0300

    feat: add meal details component

---
+++ src/components/meal-details-dialog.tsx
@@ -0,0 +1,108 @@
+import { formatDate } from "date-fns";
+import { Card, CardContent } from "./ui/card";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Badge } from "./ui/badge";
+import { UpdateMealButton } from "./update-meal-button";
+import { Button } from "./ui/button";
+import { Trash2 } from "lucide-react";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+import { DeleteMeal } from "@/api/delete-meal";
+
+interface MealDetailsProps {
+  meal: {
+    id: string;
+    name: string;
+    description: string | null;
+    time: string;
+    date: Date;
+    isOnDiet: boolean;
+  };
+}
+
+export function MealsDetailsDialog({ meal }: MealDetailsProps) {
+  const isLargeScreen = useMediaQuery("(min-width: 1024px)");
+
+  const queryClient = useQueryClient();
+
+  const { mutateAsync: deleteMeal } = useMutation({
+    mutationFn: DeleteMeal,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["meals"] });
+    },
+  });
+
+  async function handleDeleteMeal(mealId: string) {
+    try {
+      await deleteMeal({ mealId });
+      toast.success("Refeição excluída com sucesso");
+    } catch (error) {
+      console.error(error);
+      toast.error("Erro ao excluir refeição");
+    }
+  }
+
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Card className="shadow-md cursor-pointer">
+          <CardContent className="flex items-center p-4 md:p-5">
+            <span className="text-sm md:text-base">{meal.time}</span>
+            <div className="w-[1px] h-4 mx-4 bg-slate-500" />
+            <span className="flex-1 text-sm md:text-base">{meal.name}</span>
+            <div
+              className={`${
+                meal.isOnDiet ? "bg-[#E5F0DB]" : "bg-[#FDE8E8]"
+              } rounded-full size-4 md:size-5`}
+            />
+          </CardContent>
+        </Card>
+      </SheetTrigger>
+      <SheetContent
+        side={isLargeScreen ? "right" : "bottom"}
+        className="flex flex-col"
+      >
+        <SheetHeader>
+          <SheetTitle>Refeição</SheetTitle>
+        </SheetHeader>
+        <div className="p-4 md:p-5 flex-grow overflow-auto">
+          <h3 className="font-semibold text-lg">{meal.name}</h3>
+          <p className="text-sm md:text-base text-gray-500/90">
+            {meal.description || "Sem descrição"}
+          </p>
+          <h2 className="pt-4 text-md font-semibold">Data e Hora</h2>
+          <p className="text-sm md:text-base text-gray-500/90 pt-2">
+            {formatDate(new Date(meal.date), "dd/MM/yyyy")} às {meal.time}
+          </p>
+          <Badge className="bg-slate-100 px-3 py-2 rounded-full flex items-center gap-2 w-fit mt-4">
+            <div
+              className={`${
+                meal.isOnDiet ? "bg-green-500" : "bg-red-500"
+              } rounded-full size-2`}
+            />
+            <p className="text-gray-500/90">
+              {meal.isOnDiet ? "Dentro da dieta" : "Fora da dieta"}
+            </p>
+          </Badge>
+        </div>
+        <div className="flex gap-2 items-center">
+          <UpdateMealButton mealId={meal.id} mealValues={meal} />
+          <Button
+            onClick={() => handleDeleteMeal(meal.id)}
+            variant="ghost"
+            className="w-full border-2 border-neutral-900"
+          >
+            <Trash2 /> Excluir Refeição
+          </Button>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,34 +1,18 @@
-import { DeleteMeal } from "@/api/delete-meal";
 import { GetMeal } from "@/api/get-meal";
 import { GetTotalMeals } from "@/api/get-total-meals";
 import { GetTotalMealsWithinDiet } from "@/api/get-total-meals-within-diet";
 import { GamificationStatus } from "@/components/gamification-status";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
-import { useMediaQuery } from "@/hooks/useMediaQuery";
 import { calculateDietPercentage } from "@/utils/calculate-diet-percentage";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { formatDate } from "date-fns";
-import { ArrowUpRight, Trash2, Utensils } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 import { SkeletonHome } from "./sheleton-home";
 import { AddMealButton } from "@/components/add-meal-button";
-import { UpdateMealButton } from "@/components/update-meal-button";
-import { toast } from "sonner";
+import { MealsDetailsDialog } from "@/components/meal-details-dialog";
+import { ArrowUpRight, Utensils } from "lucide-react";
 
 export function Home() {
-  const isLargeScreen = useMediaQuery("(min-width: 1024px)");
-  const queryClient = useQueryClient();
-
   const { data: meals, isLoading: isMealsLoading } = useQuery({
     queryKey: ["meals"],
     queryFn: GetMeal,
@@ -46,23 +30,6 @@ export function Home() {
     queryKey: ["meals-within-diet"],
     queryFn: GetTotalMealsWithinDiet,
   });
-
-  const { mutateAsync: deleteMeal } = useMutation({
-    mutationFn: DeleteMeal,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["meals"] });
-    },
-  });
-
-  async function handleDeleteMeal(mealId: string) {
-    try {
-      await deleteMeal({ mealId });
-      toast.success("Refeição excluída com sucesso");
-    } catch (error) {
-      console.error(error);
-      toast.error("Erro ao excluir refeição");
-    }
-  }
 
   const dietPercentage = calculateDietPercentage(
     totalMeals,
@@ -126,69 +93,7 @@ export function Home() {
                   {meal.date}
                 </h2>
                 {meal.meals.map((meal, index) => (
-                  <Sheet key={index}>
-                    <SheetTrigger asChild>
-                      <Card className="shadow-md cursor-pointer">
-                        <CardContent className="flex items-center p-4 md:p-5">
-                          <span className="text-sm md:text-base">
-                            {meal.time}
-                          </span>
-                          <div className="w-[1px] h-4 mx-4 bg-slate-500" />
-                          <span className="flex-1 text-sm md:text-base">
-                            {meal.name}
-                          </span>
-                          <div
-                            className={`${
-                              meal.isOnDiet ? "bg-[#E5F0DB]" : "bg-[#FDE8E8]"
-                            } rounded-full size-4 md:size-5`}
-                          />
-                        </CardContent>
-                      </Card>
-                    </SheetTrigger>
-                    <SheetContent
-                      side={isLargeScreen ? "right" : "bottom"}
-                      className="flex flex-col"
-                    >
-                      <SheetHeader>
-                        <SheetTitle>Refeição</SheetTitle>
-                      </SheetHeader>
-                      <div className="p-4 md:p-5 flex-grow overflow-auto">
-                        <h3 className="font-semibold text-lg">{meal.name}</h3>
-                        <p className="text-sm md:text-base text-gray-500/90">
-                          {meal.description || "Sem descrição"}
-                        </p>
-                        <h2 className="pt-4 text-md font-semibold">
-                          Data e Hora
-                        </h2>
-                        <p className="text-sm md:text-base text-gray-500/90 pt-2">
-                          {formatDate(new Date(meal.date), "dd/MM/yyyy")} às{" "}
-                          {meal.time}
-                        </p>
-                        <Badge className="bg-slate-100 px-3 py-2 rounded-full flex items-center gap-2 w-fit mt-4">
-                          <div
-                            className={`${
-                              meal.isOnDiet ? "bg-green-500" : "bg-red-500"
-                            } rounded-full size-2`}
-                          />
-                          <p className="text-gray-500/90">
-                            {meal.isOnDiet
-                              ? "Dentro da dieta"
-                              : "Fora da dieta"}
-                          </p>
-                        </Badge>
-                      </div>
-                      <div className="flex gap-2 items-center">
-                        <UpdateMealButton mealId={meal.id} mealValues={meal} />
-                        <Button
-                          onClick={() => handleDeleteMeal(meal.id)}
-                          variant="ghost"
-                          className="w-full border-2 border-neutral-900"
-                        >
-                          <Trash2 /> Excluir Refeição
-                        </Button>
-                      </div>
-                    </SheetContent>
-                  </Sheet>
+                  <MealsDetailsDialog key={index} meal={meal} />
                 ))}
               </div>
             ))}
